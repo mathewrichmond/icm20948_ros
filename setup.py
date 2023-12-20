99a709a186f0d4aedb2c228d1b6f1eb118c28831
Mathew Richmond <mathewrichmond@gmail.com>
2023-12-19 19:50:13 -0800

    add qwiic_icm20948 dep

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools"],
+    install_requires=["setuptools", "qwiic_icm20948"],
     zip_safe=True,
     maintainer="mathew richmond",
     maintainer_email="mathewrichmond@gmail.com",
